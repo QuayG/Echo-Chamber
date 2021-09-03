20a27bf9733d5f6eacdabad7f8d8def401e4b590
QuayG <kaigoers@gmx.net>
2021-09-03 11:18:44 +0200

    initial commit

---
+++ backend/src/test/java/de/echochamber/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.echochamber.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
