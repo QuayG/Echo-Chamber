88cd6456dbc97d13684c8e9291442b51ac7abfce
QuayG <kaigoers@gmx.net>
2021-09-05 10:20:39 +0200

    - add package model - add UserEntity in model package - add dependencies -- Swagger dependency -- Jpa dependencies

---
+++ backend/src/main/java/de/echochamber/backend/model/UserEntity.java
@@ -0,0 +1,23 @@
+package de.echochamber.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserEntity {
+
+    @ApiModelProperty(required = true, example = "John", notes = "A users first name")
+    private String firstName;
+
+    @ApiModelProperty(required = true, example = "Doe", notes = "A users last name")
+    private String lastName;
+
+    private String role;
+
+    private String password;
+
+}
