c6ab2d46d26adf5e2539b3be903229b55d6a6974
QuayG <kaigoers@gmx.net>
2021-09-06 09:52:09 +0200

    fixed

--- backend/src/main/java/de/echochamber/backend/repo/UserRepository.java
+++ backend/src/main/java/de/echochamber/backend/repo/UserRepository.java
@@ -1,13 +1,13 @@
 package de.echochamber.backend.repo;
 
-import de.echochamber.backend.api.User;
+import de.echochamber.backend.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<User> findByName(String name);
+    Optional<UserEntity> findByFirstName(String name);
 
-    Optional<User> findByNameContains(String name);
+    Optional<UserEntity> findByFirstNameContains(String name);
 }
