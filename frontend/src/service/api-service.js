16228e4e7d1cebd764f5998511dada2c48aeedc5
QuayG <kaigoers@gmx.net>
2021-09-08 07:58:58 +0200

    - fixed initial api-service

--- frontend/src/service/api-service.js
+++ frontend/src/service/api-service.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const getToken = credentials =>
     axios
-        .post('api/auth/access_token', credentials)
+        .post('api/echo-chamber/auth/access_token', credentials)
         .then(response => response.data)
         .then(dto => dto.token)
