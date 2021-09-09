ae2dbdaf22bdfb5f7a18854c07dfb4a8d3148306
QuayG <kaigoers@gmx.net>
2021-09-09 10:17:13 +0200

    - add createUser function in api-service.js

--- frontend/src/service/api-service.js
+++ frontend/src/service/api-service.js
@@ -5,3 +5,8 @@ export const getToken = credentials =>
         .post('api/echo-chamber/auth/access_token', credentials)
         .then(response => response.data)
         .then(dto => dto.token)
+
+export const createUser = newUserInput =>
+    axios
+        .post('api/echo-chamber/user/register', newUserInput)
+        .then(response => response.data)
