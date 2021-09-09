2dc1276c831f1bff1caca36dca243b26a63e22be
QuayG <kaigoers@gmx.net>
2021-09-09 14:19:30 +0200

    - stolen styles

---
+++ frontend/src/components/GlobalStyle.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+  :root {
+    --background-dark: #111;
+    --background-light: #222;
+    --accent: #6729fa;
+    --neutral-dark: #666;
+    --neutral-light: #efefef;
+    --error: var(--accent);
+
+    --size-xs: 4px;
+    --size-s: 8px;
+    --size-m: 12px;
+    --size-l: 16px;
+    --size-xl: 24px;
+    --size-xxl: 32px;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    margin: 0;
+    font-family: monospace;
+  }
+`
