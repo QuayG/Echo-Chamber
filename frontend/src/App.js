72abc6ef6156569f991ab2cb0bde7e302772cdb4
QuayG <kaigoers@gmx.net>
2021-09-09 10:16:34 +0200

    - add Route to register page in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import AuthProvider from "./auth/AuthProvider";
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 export default function App() {
     return (
@@ -10,6 +11,9 @@ export default function App() {
                 <Switch>
                     <Route path="/login">
                         <Login/>
+                    </Route>
+                    <Route path="/register">
+                        <Register/>
                     </Route>
                     <Route exact path="/">
                         <Profile/>
