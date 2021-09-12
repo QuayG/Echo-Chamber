706da26f73f9627a766091e429b0b6f56a53855b
QuayG <kaigoers@gmx.net>
2021-09-12 09:55:05 +0200

    - changed avatar style

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,6 +1,11 @@
 import styled from "styled-components/macro";
 
 export default styled.img`
-  width: 200px;
+  width: 80%;
+  flex-grow: 1;
+  margin: 0 var(--size-l);
+  text-align: center;
+  text-decoration: none;
+  color: var(--dark-neutral);
   border-radius: 50%;
 `
