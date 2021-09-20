a73177cf7c6f8d2c19c7eadb4eb25f03d8e05fb9
QuayG <kaigoers@gmx.net>
2021-09-20 10:16:00 +0200

    - add PollsList component - add Poll component - add AnswerList component

--- frontend/src/components/Poll.js
+++ frontend/src/components/Poll.js
@@ -1,36 +1,35 @@
 import AnswerList from "./AnswerList";
 import styled from "styled-components/macro";
+import Button from "./Button";
 
 export default function Poll({poll}) {
     return (
         <Wrapper>
-            <li key={poll.title}>
-                <h3>{poll.title}</h3>
-                <AnswerList possibleAnswers={poll.possibleAnswers}/>
-                <p>No. of participants: {poll.givenAnswers.length}</p>
-                <p>Created by: {poll.user.userName}</p>
-            </li>
+            <h3>{poll.title}</h3>
+            <AnswerList possibleAnswers={poll.possibleAnswers}/>
+            <p>No. of participants: {poll.givenAnswers.length}</p>
+            <p>Created by: {poll.user.userName}</p>
+            <Button>Vote</Button>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
+  
+  height: 80%;
+  width: 260px;
+  justify-items: center;
+  justify-content: center;
+  display: grid;
+  grid-template-rows: 20% 60% 10% 10%;
+  border: 1px solid var(--accent);
+  border-radius: var(--size-s);
 
-  li {
-    margin: 20px;
-    justify-items: center;
-    align-items: center;
-    width: inherit;
-    display: grid;
-    grid-template-rows: 20% 60% 10% 10%;
-    border: 1px solid var(--accent);
-    border-radius: var(--size-s);
-
-    Button {
-      display: block;
-      width: 50%;
-      margin: 10px;
-      text-align: center;
-    }
+  Button {
+    display: block;
+    width: 50%;
+    margin: 10px;
+    text-align: center;
   }
+
 `
