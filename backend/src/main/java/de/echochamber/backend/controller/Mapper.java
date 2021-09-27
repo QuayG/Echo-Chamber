79b8b79c8a3cff33db56f7df04e0c4c0757c3e3d
QuayG <kaigoers@gmx.net>
2021-09-27 16:26:25 +0200

    - rename method

--- backend/src/main/java/de/echochamber/backend/controller/Mapper.java
+++ backend/src/main/java/de/echochamber/backend/controller/Mapper.java
@@ -105,7 +105,7 @@ abstract class Mapper {
         return polls;
     }
 
-    public Set<Poll> mapOpenPolls(Set<PollEntity> pollEntities) {
+    public Set<Poll> mapPolls(Set<PollEntity> pollEntities) {
         Set<Poll> polls = new HashSet<>();
         for (PollEntity pollEntity : pollEntities) {
             polls.add(map(pollEntity));
