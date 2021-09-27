632e52033c4f154ba6976a55a0e7eda292aeb29a
QuayG <kaigoers@gmx.net>
2021-09-27 11:36:05 +0200

    - render ReducedPollCard instead of PollCard in PollsList component

--- frontend/src/components/PollsList.js
+++ frontend/src/components/PollsList.js
@@ -1,31 +1,34 @@
-import PollCard from "./PollCard";
 import styled from "styled-components/macro";
+import ReducedPollCard from "./ReducedPollCard";
 
 export default function PollsList({polls, setPolls}) {
 
     return (
         <Wrapper>
             {polls.map(poll => (
-            <li key={poll.title}>
-                <PollCard setPolls={setPolls} poll={poll}/>
-            </li>
+                <li key={poll.title}>
+                    <ReducedPollCard setPolls={setPolls} poll={poll}/>
+                </li>
             ))}
         </Wrapper>
     )
 }
 
 const Wrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: 100%;
   width: 100%;
   padding: var(--size-s);
-  display: flex;
-  overflow-x: scroll;
+  overflow-y: scroll;
   list-style-type: none;
   text-align: center;
   text-decoration: none;
   font-size: 1em;
 
   li {
+    display: list-item;
     margin: 5px;
     padding: 5px;
   }
