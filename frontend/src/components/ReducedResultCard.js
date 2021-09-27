05d89998fde0bba5a9e7212aba971afb6e501d6d
QuayG <kaigoers@gmx.net>
2021-09-27 16:25:59 +0200

    - add ReducedResultcard component

---
+++ frontend/src/components/ReducedResultCard.js
@@ -0,0 +1,23 @@
+import styled from "styled-components/macro";
+import {Link} from "react-router-dom";
+
+export default function ReducedResultCard({poll}){
+    return (
+        <LinkStyled to={`/results/${poll.id}`}>
+            <h3>{poll.title}</h3>
+            <p>Number of participants: {poll.participants.length}</p>
+            <p>Creator: {poll.creator.userName}</p>
+            <p>Topic: TO DO</p>
+        </LinkStyled>
+    )
+}
+
+const LinkStyled = styled(Link)`
+  display: block;
+  height: min-content;
+  width: 260px;
+  color: var(--neutral-light);
+  background: var(--accent-light);
+  border: 1px solid var(--accent);
+  border-radius: var(--size-s);
+`
