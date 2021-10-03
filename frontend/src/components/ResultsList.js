c95a7a3f548c436e617c69c5ee4fe800df95a243
QuayG <kaigoers@gmx.net>
2021-10-03 02:06:43 +0200

    - refactor

--- frontend/src/components/ResultsList.js
+++ frontend/src/components/ResultsList.js
@@ -1,11 +1,11 @@
-import styled from "styled-components/macro";
 import ReducedPollCard from "./ReducedPollCard";
 import StyledLink from "./StyledLink";
+import ListWrapper from "./ListWrapper";
 
 export default function ResultsList({polls}) {
 
     return (
-        <Wrapper>
+        <ListWrapper>
             {polls.map(poll => (
                 <li key={poll.title}>
                     <StyledLink to={`/results/${poll.id}`}>
@@ -13,26 +13,6 @@ export default function ResultsList({polls}) {
                     </StyledLink>
                 </li>
             ))}
-        </Wrapper>
+        </ListWrapper>
     )
 }
-
-const Wrapper = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  padding: var(--size-s);
-  overflow-y: scroll;
-  list-style-type: none;
-  text-align: center;
-  text-decoration: none;
-  font-size: 1em;
-
-  li {
-    display: list-item;
-    margin: 5px;
-    padding: 5px;
-  }
-`
